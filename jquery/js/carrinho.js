aaf40e68dd7a4ed59d70aaa1c65323a438791639
Cristiano de Carli <cristianodecarli@gmail.com>
2016-02-04 22:36:54 -0200

    6 - Deixando sua propaganda mais bonita

--- jquery/js/carrinho.js
+++ jquery/js/carrinho.js
@@ -7,8 +7,8 @@ var umaPropaganda = function() {
   ];
   var posicao = Math.floor(propagandas.length * Math.random());
   var texto = propagandas[posicao];
-  var tr = $("<tr>").append($("<td>"));
-  tr.find("td").text(texto);
+  var tr = $("<tr>").addClass("propaganda").append($("<td>"));
+  tr.find("td").attr("colspan", 6).text(texto);
   return tr;
 };
 var atualizaDados = function() {
